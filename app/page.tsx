b7df79f6731bac33a7e9a80b8a8d2f14f0142a08
Elenilton Fabiano <eleniltonfabian@gmail.com>
2024-07-09 23:57:27 -0300

    corrigindo bug de posicionamento do footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import { Text } from "thon-ui";
 
 export default function Home(){
   return (
-    <Text variant="3xl">Hello World</Text>
+    <div className="h-[calc(100%-3.563rem)]">
+      <Text variant="3xl" >Hello World</Text>
+    </div>
+    
   );
 }
